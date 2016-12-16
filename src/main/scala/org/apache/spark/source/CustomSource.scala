aef4634ab02ccf459fb85c4f5c3d09b703cc6215
Charles Tassoni <charlestassoni@Charless-MacBook-Pro.local>
2016-12-16 11:34:25 -0500

    Added types to the metrics that CustomSource returns.

--- src/main/scala/org/apache/spark/source/CustomSource.scala
+++ src/main/scala/org/apache/spark/source/CustomSource.scala
@@ -2,7 +2,7 @@ package org.apache.spark.source
 //The Source trait is private to the org.apache.spark package, so our implementation must live here.
 
 import com.codahale.metrics.MetricRegistry
-import nl.grons.metrics.scala.InstrumentedBuilder
+import nl.grons.metrics.scala.{Counter, Histogram, InstrumentedBuilder, Timer, Gauge}
 import org.apache.spark.metrics.source.Source
 
 /**
@@ -14,12 +14,12 @@ class CustomSource(val sourceName: String) extends Source with InstrumentedBuild
 
   override val metricRegistry = new MetricRegistry
 
-  def counter(name: String) = metrics.counter(name, null)
+  def counter(name: String): Counter = metrics.counter(name, null)
 
-  def timer(name: String) = metrics.timer(name, null)
+  def timer(name: String): Timer = metrics.timer(name, null)
 
-  def histogram(name: String) = metrics.histogram(name, null)
+  def histogram(name: String): Histogram = metrics.histogram(name, null)
 
-  def gauge[T](name: String)(f: =>T) = metrics.gauge(name, null)(f)
+  def gauge[T](name: String)(f: =>T): Gauge[T] = metrics.gauge(name, null)(f)
 
 }
